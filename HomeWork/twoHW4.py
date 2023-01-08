aa8d6dde5692f7953351da2d750ec69f5c23ecf1
Your Name <you@example.com>
2023-01-08 22:58:28 +0200

    4задача решена

--- HomeWork/twoHW4.py
+++ HomeWork/twoHW4.py
@@ -3,13 +3,22 @@
 # Позиции хранятся в файле file.txt в одной строке одно число.""
 n=int(input("d:"))
 list_num=[i for i in range (-n, n+1)]
-print (list_num)
-file=open("File.txt, "r"")
+print (list_num) #создает список от -N до N
+file=open("File.txt" , "r")
 a=file.readlines()
+# for a in file:
+# print (int(a))-вывести значения из file в формате int,потом перевести в список
+
 print (a)
-multi=1
 for i in range (len(a)):
-    multi+=list_num[a[i]]
+    a[i]=int(a[i].strip())
+print(a)
+multi = 1    
+for i in range (len(a)):
+    multi*=list_num[a[i]]
 print(multi)
-list_num=list (map(str.strip,a))    
-print(list_num)
+# 
+# в 4 вы получили в "a" список со строками, к этому списку сначала нужно
+# использовать strip ко всем элементам, чтобы убрать \n, потом int чтобы
+# перевести элементы в числа.
+# Тогда в "a" у вас будут индексы, и по ним брать элементы из list_num
